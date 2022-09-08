0d768c37283715b9640680114e0bbc6ac9dd3913
SeanLin <nstk.gm@gmail.com>
2022-09-08 17:33:13 +0800

    [Docs]Mack Slide layout

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+#css writing memo:
+8 space between Normalize css,and Next.js blog's css.
+4 space between any different parts of components
--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
 
       </nav>
 
-      <div className="slide">
+      <div className="slideGroup">
         <div className='slideBtnPre'>
           <svg fill="none" viewBox="0 0 10 14"
             xmlns="http://www.w3.org/2000/svg">
@@ -96,6 +96,52 @@ export default function Home() {
           </svg>
         </div>
       </div>
+
+      <div className='stackGruop'>
+        <div className='stackBtnContainer'>
+          <button className='stackBtn'>北台灣</button>
+          <button className='stackBtn'>中台灣</button>
+          <button className='stackBtn'>南台灣</button>
+          <button className='stackBtn'>東台灣</button>
+          <button className='stackBtn'>離島</button>
+        </div>
+        <div className='stackSlide'>
+          <div className='stackPre'>
+            <svg fill="none" viewBox="0 0 10 14"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#392A93" />
+            </svg>
+          </div>
+          <div className='stackContainer'>
+            <div className='stackItemsA'>
+              <span>A</span>
+            </div>
+            <div className='stackItemsB'>
+              <span>B</span>
+            </div>
+            <div className='stackItemsC'>
+              <span>C</span>
+            </div>
+            <div className='stackItemsD'>
+              <span>D</span>
+            </div>
+            <div className='stackItemsE'>
+              <span>E</span>
+            </div>
+            <div className='stackItemsF'>
+              <span>F</span>
+            </div>
+          </div>
+          <div className='stackNex'>
+            <svg fill="none" viewBox="0 0 10 14"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#392A93" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+
 
 
       <footer>
