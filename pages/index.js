1af679845c9226079df1f023c594e09ccc7c2050
SeanLin <nstk.gm@gmail.com>
2022-09-08 10:34:32 +0800

    [Dooc]Make slide Layout

--- pages/index.js
+++ pages/index.js
@@ -76,6 +76,26 @@ export default function Home() {
 
       </nav>
 
+      <div className="slide">
+        <div className='slideBtnPre'>
+          <svg fill="none" viewBox="0 0 10 14"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#392A93" />
+          </svg>
+        </div>
+        <div className="slider">
+          <input className='slideBtns' type='radio' name="slide"></input>
+          <input className='slideBtns' type='radio' name="slide"></input>
+          <input className='slideBtns' type='radio' name="slide"></input>
+          <input className='slideBtns' type='radio' name="slide"></input>
+        </div>
+        <div className='slideBtnNex'>
+          <svg fill="none" viewBox="0 0 10 14"
+            xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#392A93" />
+          </svg>
+        </div>
+      </div>
 
 
       <footer>
@@ -93,55 +113,57 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className='footerList'>
-          <ul>
-            <a href="/#"><li>
-              關於 Taiwan 台灣
-            </li></a>
-            <a href="/#"><li>
-              我們是誰
-            </li></a>
-            <a href="/#"><li>
-              加入我們
-            </li></a>
-            <a href="/#"><li>
-              聯繫我們
-            </li></a>
-          </ul>
+        <div className="listContainer">
+          <div className='footerList'>
+            <ul>
+              <a href="/#"><li>
+                關於 Taiwan 台灣
+              </li></a>
+              <a href="/#"><li>
+                我們是誰
+              </li></a>
+              <a href="/#"><li>
+                加入我們
+              </li></a>
+              <a href="/#"><li>
+                聯繫我們
+              </li></a>
+            </ul>
+          </div>
+
+          <div className='footerList'>
+            <ul>
+              <a href="/#"><li>
+                服務條款
+              </li></a>
+              <a href="/#"><li>
+                隱私權政策
+              </li></a>
+              <a href="/#"><li>
+                ＦＡＱ
+              </li></a>
+            </ul>
+          </div>
+
+          <div className='footerList'>
+            <ul>
+              <a><li>
+                追蹤我們
+              </li></a>
+              <a href="/#"><li>
+                Facebook
+              </li></a>
+              <a href="/#"><li>
+                Instgram
+              </li></a>
+              <a href="/#"><li>
+                Line
+              </li></a>
+            </ul>
+          </div>
         </div>
 
-        <div className='footerList'>
-          <ul>
-            <a href="/#"><li>
-              服務條款
-            </li></a>
-            <a href="/#"><li>
-              隱私權政策
-            </li></a>
-            <a href="/#"><li>
-              ＦＡＱ
-            </li></a>
-          </ul>
-        </div>
-
-        <div className='footerList'>
-          <ul>
-            <a><li>
-              追蹤我們
-            </li></a>
-            <a href="/#"><li>
-              Facebook
-            </li></a>
-            <a href="/#"><li>
-              Instgram
-            </li></a>
-            <a href="/#"><li>
-              Line
-            </li></a>
-          </ul>
-        </div>
       </footer>
-
     </div >
   )
 }
