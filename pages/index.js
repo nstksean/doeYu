fc702c84caac7b5b2de2a1a0a1a997337c4a26ef
SeanLin <nstk.gm@gmail.com>
2022-09-12 00:07:15 +0800

    [Docs]Make stacks,hotSpots,hotFood,subscript.

--- pages/index.js
+++ pages/index.js
@@ -98,50 +98,153 @@ export default function Home() {
       </div>
 
       <div className='stackGruop'>
+
         <div className='stackBtnContainer'>
-          <button className='stackBtn'>北台灣</button>
-          <button className='stackBtn'>中台灣</button>
-          <button className='stackBtn'>南台灣</button>
-          <button className='stackBtn'>東台灣</button>
-          <button className='stackBtn'>離島</button>
+
+          <div className='stackBtn'>
+            <label className='stackBtnLb' htmlFor="北台灣">北台灣</label>
+            <input type='radio' name="city" id="北台灣" />
+          </div>
+          <div className='stackBtn'>
+            <label className='stackBtnLb' htmlFor="中台灣">中台灣</label>
+            <input type='radio' name="city" id="中台灣" />
+          </div>
+          <div className='stackBtn'>
+            <input type='radio' name="city" id="南台灣" />
+            <label className='stackBtnLb' htmlFor="南台灣">南台灣</label>
+          </div>
+          <div className='stackBtn'>
+            <label className='stackBtnLb' htmlFor="東台灣">東台灣</label>
+            <input type='radio' name="city" id="東台灣" />
+          </div>
+          <div className='stackBtn'>
+            <label className='stackBtnLb' htmlFor="離島">離島</label>
+            <input type='radio' name="city" id="離島" />
+          </div>
+
         </div>
         <div className='stackSlide'>
           <div className='stackPre'>
             <svg fill="none" viewBox="0 0 10 14"
               xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#392A93" />
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#FA7E5F" />
             </svg>
           </div>
           <div className='stackContainer'>
-            <div className='stackItemsA'>
+            <div className='stackItems'>
               <span>A</span>
             </div>
-            <div className='stackItemsB'>
+            <div className='stackItems'>
               <span>B</span>
             </div>
-            <div className='stackItemsC'>
+            <div className='stackItems'>
               <span>C</span>
             </div>
-            <div className='stackItemsD'>
+            <div className='stackItems'>
               <span>D</span>
             </div>
-            <div className='stackItemsE'>
+            <div className='stackItems'>
               <span>E</span>
             </div>
-            <div className='stackItemsF'>
+            <div className='stackItems'>
               <span>F</span>
             </div>
           </div>
           <div className='stackNex'>
             <svg fill="none" viewBox="0 0 10 14"
               xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#392A93" />
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#FA7E5F" />
             </svg>
           </div>
         </div>
       </div>
 
+      <div className='hotSpotGroup'>
+        <div className='title'>
+          熱門景點
+        </div>
+        <div className='hotSpotBtnContainer'>
+          <button className='hotSpotBtnPre'>
+            <svg fill="none" viewBox="0 0 10 14"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#FEFEFE" />
+            </svg>
+          </button>
+          <button className='hotSpotBtnNex'>
+            <svg fill="none" viewBox="0 0 10 14"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.200059 13.6001C-0.131312 13.1582 -0.0417684 12.5314 0.400059 12.2001L7.33339 7.00006L0.400059 1.80006C-0.0417689 1.46869 -0.131312 0.841886 0.200059 0.400059C0.53143 -0.0417692 1.15823 -0.131312 1.60006 0.200059L9.60006 6.20006C9.85186 6.38891 10.0001 6.6853 10.0001 7.00006C10.0001 7.31482 9.85186 7.6112 9.60006 7.80006L1.60006 13.8001C1.15823 14.1314 0.53143 14.0419 0.200059 13.6001Z" fill="#FEFEFE" />
+            </svg>
+          </button>
+        </div>
+        <div className='hotSpotStackContainer'>
+          <div className='hotSpotStackItem'>
+            <div className='ItemPhoto'>imphoto</div>
+            <div className="spotTitle">我是景點名稱</div>
+            <div className='spotCity'>我是城市</div>
+          </div>
+          <div className='hotSpotStackItem'>
+            <div className='ItemPhoto'>imphoto</div>
+            <div className="spotTitle">我是景點名稱</div>
+            <div className='spotCity'>我是城市</div>
+          </div>
+          <div className='hotSpotStackItem'>
+            <div className='ItemPhoto'>imphoto</div>
+            <div className="spotTitle">我是景點名稱</div>
+            <div className='spotCity'>我是城市</div>
+          </div>
+          <div className='hotSpotStackItem'>
+            <div className='ItemPhoto'>imphoto</div>
+            <div className="spotTitle">我是景點名稱</div>
+            <div className='spotCity'>我是城市</div>
+          </div>
+        </div>
+      </div>
 
+      <div className='hotFoodGroup'>
+        <div className='title'>
+          人氣美食
+        </div>
+        <div className='stackFoodContainer'>
+          <div className='foodStackItem'>
+            1
+          </div>
+          <div className='foodStackItem'>
+            2
+          </div>
+          <div className='foodStackItem'>
+            3
+          </div>
+          <div className='foodStackItem'>
+            4
+          </div>
+          <div className='foodStackItem'>
+            5
+          </div>
+          <div className='foodStackItem'>
+            6
+          </div>
+
+
+        </div>
+      </div>
+
+      <form>
+        <div className='subscriptGruop'>
+
+          <div className='title'>
+            訂閱我們，獲得最在地的旅遊資訊！
+          </div>
+          <div className='subTitle'>
+            每週六一封，不隨意打擾，且隨時可以取消
+          </div>
+          <div className='inputer'>
+            <input type="e-mail" placeholder='輸入你的 Email' />
+            <button type='submit'>訂閱</button>
+          </div>
+
+        </div>
+      </form>
 
 
       <footer>
