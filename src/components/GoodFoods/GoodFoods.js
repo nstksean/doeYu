f28601c0f7e90fe822be967431f34ef1cbeb4d02
SeanLin <nstk.gm@gmail.com>
2022-10-10 21:33:37 +0800

    docs(component):Add static photo in goodFood
    #7

--- src/components/GoodFoods/GoodFoods.js
+++ src/components/GoodFoods/GoodFoods.js
@@ -1,5 +1,7 @@
 import { RootContext } from "../../context/rustContext";
 import { useContext } from "react";
+import { foodTypes } from "./FoodItems";
+import Image from "next/image";
 
 import style from "./goodfoods.module.css"
 
@@ -9,42 +11,97 @@ export default function GoodFoods() {
     const datas = rootContextValue.state.rustAPI_data
     const loading = rootContextValue.state.rustAPI_isLoaded
 
-    console.log({ datas, loading });
-
     return (
         <div className={style.hotFoodGroup}>
             <div className={style.title}>
                 人氣美食
             </div>
             <div className={style.stackFoodContainer}>
-                {
-                    datas?.slice(0, 6).map((data) =>
-                        <div key={data.ScenicSpotID}
-                            className={style.foodStackItem}>
-                            <div className={style.foodType}> {data.ScenicSpotName}</div>
-                        </div>
-                    )
+                <div className={style.foodStackItem}>
+                    <div className={style.bgWrap}>
+                        <Image
+                            alt={foodTypes.at(0).name}
+                            src={foodTypes.at(0).imageUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className={style.foodType}>
+                        {foodTypes.at(0).name}
+                    </p>
+                </div>
+                <div className={style.foodStackItem}>
+                    <div className={style.bgWrap}>
+                        <Image
+                            alt={foodTypes.at(1).name}
+                            src={foodTypes.at(1).imageUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className={style.foodType}>
+                        {foodTypes.at(1).name}
+                    </p>
+                </div>
+                <div className={style.foodStackItem}>
+                    <div className={style.bgWrap}>
+                        <Image
+                            alt={foodTypes.at(2).name}
+                            src={foodTypes.at(2).imageUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className={style.foodType}>
+                        {foodTypes.at(2).name}
+                    </p>
+                </div>
+                <div className={style.foodStackItem}>
+                    <div className={style.bgWrap}>
+                        <Image
+                            alt={foodTypes.at(3).name}
+                            src={foodTypes.at(3).imageUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className={style.foodType}>
+                        {foodTypes.at(3).name}
+                    </p>
+                </div>
+                <div className={style.foodStackItem}>
+                    <div className={style.bgWrap}>
+                        <Image
+                            alt={foodTypes.at(4).name}
+                            src={foodTypes.at(4).imageUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className={style.foodType}>
+                        {foodTypes.at(4).name}
+                    </p>
+                </div>
+                <div className={style.foodStackItem}>
+                    <div className={style.bgWrap}>
+                        <Image
+                            alt={foodTypes.at(5).name}
+                            src={foodTypes.at(5).imageUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                        />
+                    </div>
+                    <p className={style.foodType}>
+                        {foodTypes.at(5).name}
+                    </p>
+                </div>
 
-                }
-
-                {/* <div className={style.foodStackItem}>
-                    <div className={style.foodType}>中式美食</div>
-                </div> 
-                <div className={style.foodStackItem}>
-                    <div className={style.foodType}>夜市小吃</div>
-                </div>
-                <div className={style.foodStackItem}>
-                    <div className={style.foodType}>異國料理</div>
-                </div>
-                <div className={style.foodStackItem}>
-                    <div className={style.foodType}>甜點冰品</div>
-                </div>
-                <div className={style.foodStackItem}>
-                    <div className={style.foodType}>伴手禮</div>
-                </div>
-                <div className={style.foodStackItem}>
-                    <div className={style.foodType}>全部類別</div>
-                </div> */}
 
             </div>
 
